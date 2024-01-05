1b373f921f6e844b9709edc53d3b169fa2981c29
Yessen Murat <shoyo2209@gmail.com>
2024-01-05 19:04:03 +0600

    refactor: add zoom effect on image hover

--- src/components/GalleryImageCard.vue
+++ src/components/GalleryImageCard.vue
@@ -12,12 +12,14 @@ defineProps({
 
 <template>
   <article class="image-card">
-    <img
-      :src="photoInfo.urls.regular"
-      :alt="photoInfo.description || photoInfo.alt_description"
-      loading="lazy"
-      class="image-card__image"
-    />
+    <div class="image-card__image-container">
+      <img
+        :src="photoInfo.urls.regular"
+        :alt="photoInfo.description || photoInfo.alt_description"
+        loading="lazy"
+        class="image-card__image"
+      />
+    </div>
     <div class="image-card__body">
       <GalleryImageMetric :likes="photoInfo.likes" />
       <GalleryImageAuthor :author="photoInfo.user" />
@@ -29,15 +31,30 @@ defineProps({
 .image-card {
   border-radius: $radius-md;
 
-  &__image {
+  &__image-container {
     @extend .image-card;
+    position: relative;
+    overflow: hidden;
     width: 100%;
     height: 400px;
+    cursor: pointer;
+
+    &:hover img {
+      transform: scale(1.1);
+      transform-origin: 50% 50%;
+    }
+  }
+
+  &__image {
+    @extend .image-card;
+    width: inherit;
+    height: inherit;
     object-fit: cover;
+    transition: transform 300ms;
   }
 
   &__body {
-    padding: $spacing-xxs $spacing-md;
+    padding: $spacing-md;
     @include flex;
     @include justify-content;
   }
