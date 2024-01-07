5736b91461d9890d6a41f33fb33098382edb06a0
Yessen Murat <shoyo2209@gmail.com>
2024-01-08 05:17:51 +0600

    chore: implement image download

--- src/views/ImageDetailsView.vue
+++ src/views/ImageDetailsView.vue
@@ -2,35 +2,67 @@
 import { onBeforeMount, ref } from 'vue';
 import GalleryImageAuthor from '@/components/GalleryImageAuthor.vue';
 import api from '@/config/api';
+import BaseLoader from '@/components/common/BaseLoader.vue';
+import HeartIcon from '@/components/icons/HeartIcon.vue';
 
 const props = defineProps({
   id: String,
 });
 
 let photoInfo = ref({});
+let loading = ref(false);
 
 onBeforeMount(() => {
   getImageDetails();
 });
 
 async function getImageDetails() {
+  loading.value = true;
+
   try {
     const response = await api.get(`/photos/${props.id}`);
     photoInfo.value = response.data;
   } catch (error) {
     console.error(error);
+  } finally {
+    loading.value = false;
   }
+}
+
+function toDataURL(url) {
+  return fetch(url)
+    .then((response) => {
+      return response.blob();
+    })
+    .then((blob) => {
+      return window.URL.createObjectURL(blob);
+    });
+}
+
+async function downloadImage() {
+  const a = document.createElement('a');
+  a.href = await toDataURL(photoInfo.value.urls.full);
+  a.download = `photo-${props.id}.png`;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
 }
 </script>
 
 <template>
-  <main>
+  <BaseLoader v-if="loading" />
+  <main v-else>
     <div class="image-overlay">
       <img :src="photoInfo.urls.full" alt="" />
     </div>
     <section class="image-details">
       <header class="image-details__header">
         <GalleryImageAuthor :author="photoInfo.user" />
+
+        <div class="image-details__actions">
+          <button><HeartIcon /></button>
+          <button @click="downloadImage">Скачать</button>
+        </div>
       </header>
       <div class="image-container">
         <img :src="photoInfo.urls.raw" alt="" />
@@ -40,6 +72,9 @@ async function getImageDetails() {
 </template>
 
 <style lang="scss" scoped>
+main {
+  position: relative;
+}
 .image-overlay {
   position: absolute;
   width: 100%;
@@ -66,6 +101,28 @@ async function getImageDetails() {
 
   &__header {
     margin-top: $spacing-5xl;
+    @include flex;
+    @include justify-content;
+    @include align-items;
+  }
+
+  &__actions {
+    @include flex;
+    gap: $spacing-lg;
+
+    button {
+      padding: $spacing-md;
+      background-color: $white;
+      border-radius: $radius-md;
+      box-shadow: $shadow-default;
+      cursor: pointer;
+    }
+
+    button:nth-child(1) {
+      svg {
+        width: 1.25rem;
+      }
+    }
   }
 }
 
