034295571fcc8dd21e2624de55bfa4547148929e
Yessen Murat <shoyo2209@gmail.com>
2024-01-07 23:49:42 +0600

    refactor: add search photos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,18 +7,44 @@ import api, { ACCESS_KEY } from '@/config/api';
 let photoList = ref([]);
 let page = ref(1);
 let loading = ref(false);
+let searchQuery = ref('');
 
 onBeforeMount(() => {
   getPhotos();
   window.addEventListener('scroll', handleScroll);
 });
 
-async function getPhotos() {
+const debounce = (fn, delay) => {
+  let timeoutId;
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
+};
+
+const debouncedSearch = debounce(() => {
+  loading.value = true;
+  getPhotos(searchQuery.value);
+  photoList.value = [];
+  page.value = 1;
+  loading.value = false;
+}, 500);
+
+async function getPhotos(searchQuery = '') {
   loading.value = true;
 
+  const apiUrl = searchQuery.trim()
+    ? `/search/photos?query=${encodeURIComponent(searchQuery)}&page=${
+        page.value
+      }&per_page=9&client_id=${ACCESS_KEY}`
+    : `/photos?page=${page.value}&per_page=9&client_id=${ACCESS_KEY}`;
+
   try {
-    const response = await api.get(`/photos?page=${page.value}&per_page=9&client_id=${ACCESS_KEY}`);
-    photoList.value = [...photoList.value, ...response.data];
+    const response = await api.get(apiUrl);
+    const responseData = searchQuery.trim() ? response.data.results : response.data;
+    photoList.value = [...photoList.value, ...responseData];
     page.value++;
   } catch (error) {
     console.error(error);
@@ -29,7 +55,7 @@ async function getPhotos() {
 
 const handleScroll = () => {
   if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100 && !loading.value) {
-    getPhotos();
+    getPhotos(searchQuery.value);
   }
 };
 
@@ -41,6 +67,9 @@ onBeforeUnmount(() => {
 <template>
   <h1>Gallery app</h1>
   <main>
+    <section>
+      <input type="text" v-model="searchQuery" placeholder="Search..." @input="debouncedSearch" />
+    </section>
     <GalleryImageGrid>
       <GalleryImageCard v-for="photo in photoList" :key="photo.id" :photoInfo="photo" />
     </GalleryImageGrid>
