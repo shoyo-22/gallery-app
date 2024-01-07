af07737d55ca87021318bb04ad71e350fb628e49
Yessen Murat <shoyo2209@gmail.com>
2024-01-07 23:32:21 +0600

    refactor: implement infinte scroll

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,41 @@
 <script setup>
-import { onBeforeMount, ref } from 'vue';
+import { onBeforeMount, onBeforeUnmount, ref } from 'vue';
 import GalleryImageCard from '@/components/GalleryImageCard.vue';
 import GalleryImageGrid from '@/components/GalleryImageGrid.vue';
 import api, { ACCESS_KEY } from '@/config/api';
 
 let photoList = ref([]);
+let page = ref(1);
+let loading = ref(false);
 
 onBeforeMount(() => {
   getPhotos();
+  window.addEventListener('scroll', handleScroll);
 });
 
 async function getPhotos() {
+  loading.value = true;
+
   try {
-    const response = await api.get(`/photos?page=1&per_page=9&client_id=${ACCESS_KEY}`);
-    photoList.value = response.data;
+    const response = await api.get(`/photos?page=${page.value}&per_page=9&client_id=${ACCESS_KEY}`);
+    photoList.value = [...photoList.value, ...response.data];
+    page.value++;
   } catch (error) {
     console.error(error);
+  } finally {
+    loading.value = false;
   }
 }
+
+const handleScroll = () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100 && !loading.value) {
+    getPhotos();
+  }
+};
+
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', handleScroll);
+});
 </script>
 
 <template>
@@ -27,4 +45,5 @@ async function getPhotos() {
       <GalleryImageCard v-for="photo in photoList" :key="photo.id" :photoInfo="photo" />
     </GalleryImageGrid>
   </main>
+  <div v-if="loading" style="text-align: center">Loading...</div>
 </template>
