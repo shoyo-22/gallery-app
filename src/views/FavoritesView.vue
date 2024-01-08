c044e39f6e75fbe3cc896ba1bc02bf87f3df7203
Yessen Murat <shoyo2209@gmail.com>
2024-01-08 06:13:58 +0600

    refactor: show favorite photos
    load ids from local storage and make api call inside loop

--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -1,3 +1,65 @@
+<script setup>
+import { ref, onBeforeMount } from 'vue';
+import api from '@/config/api';
+import BaseLoader from '@/components/common/BaseLoader.vue';
+import GalleryImageCard from '@/components/GalleryImageCard.vue';
+import GalleryImageGrid from '@/components/GalleryImageGrid.vue';
+import ScrollToTopBtn from '@/components/common/ScrollToTopBtn.vue';
+
+let favoriteList = ref([]);
+let photoList = ref([]);
+let loading = ref(false);
+
+function getFavoriteList() {
+  const storedData = localStorage.getItem('favoriteList');
+  favoriteList.value = storedData ? JSON.parse(storedData) : [];
+}
+
+onBeforeMount(() => {
+  getFavoriteList();
+  getPhotos();
+});
+
+async function getPhotos() {
+  if (!favoriteList.value.length) {
+    return [];
+  }
+
+  loading.value = true;
+  for (const photoId of favoriteList.value) {
+    try {
+      const response = await api.get(`/photos/${photoId}`);
+      photoList.value.push(response.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      loading.value = false;
+    }
+  }
+}
+</script>
+
 <template>
-  <div>Favirotes Page</div>
+  <h1>Избранное</h1>
+  <p v-if="!favoriteList.length">Список пуст</p>
+  <main v-else>
+    <BaseLoader v-if="loading" />
+    <GalleryImageGrid v-else>
+      <GalleryImageCard v-for="photo in photoList" :key="photo.id" :photoInfo="photo" />
+    </GalleryImageGrid>
+  </main>
+  <ScrollToTopBtn />
 </template>
+
+<style lang="scss" scoped>
+h1,
+p {
+  text-align: center;
+}
+
+h1 {
+  margin: $spacing-xl 0;
+  font-size: 2rem;
+  font-weight: 700;
+}
+</style>
